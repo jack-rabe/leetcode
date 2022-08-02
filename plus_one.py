ab02b4b8aa3bdf98350a28de27ed4b92a970eaa9
= <jhrabe@wisc.edu>
2022-08-02 16:36:11 -0500

    66 - alternate soln

--- plus_one.py
+++ plus_one.py
@@ -1,14 +1,9 @@
 class Solution(object):
     def plusOne(self, digits):
-        for i in range(len(digits) - 1, -1, -1):
-            digits[i] += 1
-            if digits[i] != 10:
-                break
-
-            digits[i] = 0
-            if i == 0:
-                digits.insert(0, 1)
-        return digits
-
-
-print(Solution().plusOne([9, 9, 9]))
+        nums = []
+        for digit in digits:
+            nums.append(str(digit))
+        ans = int("".join(nums))
+        ans += 1
+        ans = str(ans)
+        return [*ans]
